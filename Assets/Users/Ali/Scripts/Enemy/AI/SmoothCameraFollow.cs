052b3f19bb8423340b194b50555dd67baaccdb89
Ali-Durman <99588223+Ali-Durman@users.noreply.github.com>
2023-01-12 00:50:05 +0300

    Created: Scripts
    Created: All AI Moves, Smooth Camera Follow, Retreat (Half).

---
+++ Assets/Users/Ali/Scripts/Enemy/AI/SmoothCameraFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPos;
+
+        transform.LookAt(target);
+    }
+}
